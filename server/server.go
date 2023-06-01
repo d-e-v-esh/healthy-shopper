792c92a0fea198a11b1e5a98a4dfea388a8ef086
Devesh <deves125@gmail.com>
2023-06-01 18:01:36 +0530

    feat: connect to database

--- server/server.go
+++ server/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -8,11 +10,21 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/d-e-v-esh/healthy-shopper/graph"
+	"github.com/jackc/pgx/v5"
 )
 
 const defaultPort = "8080"
 
 func main() {
+
+	databaseConnectionString := "postgres://postgres:postgres@localhost:5432/HealthyShopper?sslmode=disable"
+	conn, err := pgx.Connect(context.Background(), databaseConnectionString)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
