d7d1a4de0237ee0b40e3a3fd7229de4166d8fc52
Devesh <deves125@gmail.com>
2023-06-02 16:20:57 +0530

    fix: update schema

--- server/migrations/1_initialize_schema.up.sql
+++ server/migrations/1_initialize_schema.up.sql
@@ -10,14 +10,14 @@ CREATE TABLE "User" (
 );
 
 CREATE TABLE "UserAddress" (
-  "UserID" int,
-  "AddressID" int,
-  "IsDefault" boolean
+  "UserID" int NOT NULL,
+  "AddressID" int NOT NULL,
+  "IsDefault" boolean NOT NULL
 );
 
 CREATE TABLE "Address" (
   "AddressID" int PRIMARY KEY,
-  "PhoneNumber" int NOT NULL,
+  "PhoneNumber" varchar(20) NOT NULL,
   "AddressLine1" varchar(100) NOT NULL,
   "AddressLine2" varchar(100) NOT NULL,
   "City" varchar(35) NOT NULL,
@@ -27,13 +27,13 @@ CREATE TABLE "Address" (
 
 CREATE TABLE "Product" (
   "ProductID" int PRIMARY KEY,
-  "ProductCategoryID" int NOT NULL,
+  "ProductCategoryID" int UNIQUE,
   "Name" varchar(50) NOT NULL,
   "Description" varchar(500) NOT NULL,
   "ProductImage" varchar(500) NOT NULL,
-  "IngredientsListID" int UNIQUE NOT NULL,
-  "NutritionalInformationID" int UNIQUE NOT NULL,
-  "PromotionID" int UNIQUE NOT NULL,
+  "IngredientsListID" int UNIQUE,
+  "NutritionalInformationID" int UNIQUE,
+  "PromotionID" int UNIQUE,
   "CreatedAt" timestamptz DEFAULT (now()),
   "UpdatedAt" timestamptz
 );
@@ -41,36 +41,36 @@ CREATE TABLE "Product" (
 CREATE TABLE "NutritionalInformation" (
   "NutritionalInformationID" int PRIMARY KEY,
   "NutritionalInformationListID" int UNIQUE NOT NULL,
-  "NAmount" int,
-  "NMeasuringUnit" varchar(6)
+  "NAmount" int NOT NULL,
+  "NMeasuringUnit" varchar(6) NOT NULL
 );
 
 CREATE TABLE "NutritionalInformationList" (
   "NutritionalInformationListID" int PRIMARY KEY,
-  "Parameter" varchar(20),
-  "PerNAmount" float4,
-  "MeasuringUnit" varchar(6)
+  "Parameter" varchar(20) NOT NULL,
+  "PerNAmount" float4 NOT NULL,
+  "MeasuringUnit" varchar(6) NOT NULL
 );
 
 CREATE TABLE "IngredientsList" (
   "IngredientsListID" int PRIMARY KEY,
-  "Name" varchar(100),
-  "Description" varchar(500)
+  "Name" varchar(100) NOT NULL,
+  "Description" varchar(500) NOT NULL
 );
 
 CREATE TABLE "ProductItem" (
   "ProductItemID" int PRIMARY KEY,
   "ProductID" int UNIQUE NOT NULL,
-  "StockKeepingUnit" varchar(100),
-  "QuantityInStock" int,
-  "ProductImage" varchar(500),
-  "Price" float4
+  "StockKeepingUnit" varchar(100) NOT NULL,
+  "QuantityInStock" int NOT NULL,
+  "ProductImage" varchar(500) NOT NULL,
+  "Price" float4 NOT NULL
 );
 
 CREATE TABLE "ProductCategory" (
   "ProductCategoryID" int PRIMARY KEY,
   "ParentCategoryID" int UNIQUE NOT NULL,
-  "CategoryName" varchar(20)
+  "CategoryName" varchar(20) NOT NULL
 );
 
 CREATE TABLE "ProductConfiguration" (
@@ -80,23 +80,23 @@ CREATE TABLE "ProductConfiguration" (
 
 CREATE TABLE "Variation" (
   "VariationID" int PRIMARY KEY,
-  "ProductCategoryID" int UNIQUE NOT NULL,
-  "Name" varchar(20)
+  "ProductCategoryID" int UNIQUE,
+  "Name" varchar(20) NOT NULL
 );
 
 CREATE TABLE "VariationOption" (
   "VariationOptionID" int PRIMARY KEY,
   "VariationID" int UNIQUE NOT NULL,
-  "Value" varchar(50)
+  "Value" varchar(50) NOT NULL
 );
 
 CREATE TABLE "Promotion" (
   "PromotionID" int PRIMARY KEY,
-  "Name" varchar(100),
-  "Description" varchar(500),
-  "DiscountRate" int,
-  "StartDate" timestamptz,
-  "EndDate" timestamptz
+  "Name" varchar(100) NOT NULL,
+  "Description" varchar(500) NOT NULL,
+  "DiscountRate" int NOT NULL,
+  "StartDate" timestamptz NOT NULL,
+  "EndDate" timestamptz NOT NULL
 );
 
 CREATE TABLE "ShoppingCart" (
@@ -106,57 +106,57 @@ CREATE TABLE "ShoppingCart" (
 
 CREATE TABLE "ShoppingCartItem" (
   "ShoppingCartItemID" int PRIMARY KEY,
-  "ShoppingCartID" int,
-  "ProductItemID" int,
-  "Quantity" int
+  "ShoppingCartID" int UNIQUE NOT NULL,
+  "ProductItemID" int UNIQUE NOT NULL,
+  "Quantity" int NOT NULL
 );
 
 CREATE TABLE "ShopOrder" (
   "ShopOrderID" int PRIMARY KEY,
   "UserID" int UNIQUE NOT NULL,
-  "OrderDateAndTime" timestamptz DEFAULT (now()),
-  "PaymentMethod" varchar(20),
+  "OrderDateAndTime" timestamptz NOT NULL DEFAULT (now()),
+  "PaymentMethod" varchar(20) NOT NULL,
   "ShippingAddressID" int UNIQUE NOT NULL,
   "ShippingMethodID" int UNIQUE NOT NULL,
-  "OrderTotal" float4,
+  "OrderTotal" float4 NOT NULL,
   "OrderStatusID" int UNIQUE NOT NULL
 );
 
 CREATE TABLE "ShippingAddress" (
   "ShippingAddressID" int PRIMARY KEY,
-  "PhoneNumber" int,
-  "AddressLine1" varchar(100),
-  "AddressLine2" varchar(100),
-  "City" varchar(35),
-  "State" varchar(20),
-  "PINCode" varchar(10)
+  "PhoneNumber" varchar(20) NOT NULL,
+  "AddressLine1" varchar(100) NOT NULL,
+  "AddressLine2" varchar(100) NOT NULL,
+  "City" varchar(35) NOT NULL,
+  "State" varchar(20) NOT NULL,
+  "PINCode" varchar(10) NOT NULL
 );
 
 CREATE TABLE "ShippingMethod" (
   "ShippingMethodID" int PRIMARY KEY,
-  "Name" varchar(25),
-  "Price" float4
+  "Name" varchar(25) NOT NULL,
+  "Price" float4 NOT NULL
 );
 
 CREATE TABLE "OrderStatus" (
   "OrderStatusID" int PRIMARY KEY,
-  "Status" varchar(20)
+  "Status" varchar(20) NOT NULL
 );
 
 CREATE TABLE "OrderLine" (
   "OrderLineID" int PRIMARY KEY,
-  "ProductItemID" int UNIQUE NOT NULL,
-  "ShopOrderID" int UNIQUE NOT NULL,
-  "Quantity" int,
-  "Price" float4
+  "ProductItemID" int UNIQUE,
+  "ShopOrderID" int UNIQUE,
+  "Quantity" int NOT NULL,
+  "Price" float4 NOT NULL
 );
 
 CREATE TABLE "UserReview" (
   "UserReviewID" int PRIMARY KEY,
   "UserID" int UNIQUE NOT NULL,
   "OrderedProductID" int UNIQUE NOT NULL,
-  "RatingValue" int,
-  "Comment" varchar(500)
+  "RatingValue" int NOT NULL,
+  "Comment" varchar(500) NOT NULL
 );
 
 ALTER TABLE "UserAddress" ADD FOREIGN KEY ("UserID") REFERENCES "User" ("UserID");
