da32d1afa0dfdc81a26eea837d339cf0da22d824
Devesh <deves125@gmail.com>
2023-06-03 12:44:21 +0530

    feat: add annotations for user and product  - Adds Query and Mutation (creation) capabilities for our schemas

--- server/ent/schema/product.go
+++ server/ent/schema/product.go
@@ -1,6 +1,10 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"entgo.io/contrib/entgql"
+	"entgo.io/ent"
+	"entgo.io/ent/schema"
+)
 
 // Product holds the schema definition for the Product entity.
 type Product struct {
@@ -15,4 +19,11 @@ func (Product) Fields() []ent.Field {
 // Edges of the Product.
 func (Product) Edges() []ent.Edge {
 	return nil
+}
+
+func (Product) Annotations() []schema.Annotation {
+	return []schema.Annotation{
+		entgql.QueryField(),
+		entgql.Mutations(entgql.MutationCreate()),
+	}
 }
