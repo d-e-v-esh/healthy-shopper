efef7434176b80e6dd07f10e0696eb26b0ade8e1
Devesh <deves125@gmail.com>
2023-06-03 21:36:52 +0530

    feat: add fields to product schema

--- server/ent/schema/product.go
+++ server/ent/schema/product.go
@@ -1,9 +1,12 @@
 package schema
 
 import (
+	"time"
+
 	"entgo.io/contrib/entgql"
 	"entgo.io/ent"
 	"entgo.io/ent/schema"
+	"entgo.io/ent/schema/field"
 )
 
 // Product holds the schema definition for the Product entity.
@@ -13,7 +16,19 @@ type Product struct {
 
 // Fields of the Product.
 func (Product) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.String("Name").NotEmpty(),
+		field.String("Description").NotEmpty(),
+		field.String("ProductImage").NotEmpty(),
+		field.Int("ProductCategoryID").Positive(),
+		field.Int("IngredientsListID").Positive(),
+		field.Int("NutritionalInformationID").Positive(),
+		field.Int("PromotionID").Positive(),
+
+		field.Time("CreatedAt").Default(time.Now).Immutable(),
+		field.Time("UpdatedAt").Default(time.Now).UpdateDefault(time.Now),
+	}
+
 }
 
 // Edges of the Product.
