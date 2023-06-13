20e21d3a801053a8daff8da24236ffdd3bdbe0f4
Devesh <deves125@gmail.com>
2023-06-14 00:11:25 +0530

    fix: username min limit from 4 to 3

--- server/pkg/validation/validate_register_input.go
+++ server/pkg/validation/validate_register_input.go
@@ -10,7 +10,7 @@ import (
 type RegisterInput struct {
 	FirstName    string `validate:"required,alpha,min=3,max=50"`
 	LastName     string `validate:"required,alpha,min=3,max=50"`
-	Username     string `validate:"required,username,min=4,max=20"`
+	Username     string `validate:"required,username,min=3,max=20"`
 	EmailAddress string `validate:"required,email"`
 	Password     string `validate:"required,password,min=8,max=100"`
 }
@@ -19,7 +19,7 @@ type RegisterInput struct {
 var customFieldErrors = map[string]string{
 	"RegisterInput.FirstName":    "First name is required, must be alphabetical, and between 3 to 50 characters.",
 	"RegisterInput.LastName":     "Last name is required, must be alphabetical, and between 3 to 50 characters.",
-	"RegisterInput.Username":     "Username is required, can only contain letters, numbers, underscores, and dashes, and must be between 4 to 20 characters.",
+	"RegisterInput.Username":     "Username is required, can only contain letters, numbers, underscores, and dashes, and must be between 3 to 20 characters.",
 	"RegisterInput.EmailAddress": "A valid email address is required.",
 	"RegisterInput.Password":     "Password is required, must be at least 8 characters long, and contain at least 1 lowercase letter, 1 uppercase letter, 1 number, and 1 symbol.",
 }
